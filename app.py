5d724beff5782cb24859e71efdebbf593f3f9047
alexm <alex-m@outlook.com>
2023-01-10 16:01:11 -0800

    Add Flask and debug toolbar imports; setup app and debug toolbar; initialize empty list to hold user responses to survey

--- app.py
+++ app.py
@@ -4,3 +4,14 @@
 """
 Main code for survey application - Flask setup, routes, and view functions.
 """
+
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "alt ceva secreta"
+
+debug = DebugToolbarExtension(app)
+
+
+responses = []  # Stores user responses to questions
